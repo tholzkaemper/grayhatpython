cdd99ddd475671ca5f64b4066f7c36f267394a46
DESKTOP-IHBJ9ED\KaiserWodka <tim.holzkaemper@gmail.com>
2017-11-19 00:58:50 +0100

    code from chapter 3.3

--- chapter3/mydebugger/my_debugger.py
+++ chapter3/mydebugger/my_debugger.py
@@ -8,6 +8,10 @@ class debugger():
         self.h_process = None
         self.pid = None
         self.debugger_active = False
+        self.h_thread = None
+        self.context = None
+        self.exception = None
+        self.exception_address = None
 
     def load(self, path_to_exe):
 
@@ -79,9 +83,31 @@ class debugger():
 
         if kernel32.WaitForDebugEvent(byref(debug_event), INFINITE):
 
-            # Not building event handlers yet
-            #input("Press a key to continue...")
-            #self.debugger_active = False
+            # Get thread and context information
+            self.h_thread = self.open_thread(debug_event.dwThreadId)
+            self.context = self.get_thread_context(h_thread=self.h_thread)
+
+            print("Event Code: %d Thread ID: %d" %
+                  (debug_event.dwDebugEventCode, debug_event.dwThreadId))
+
+            if debug_event.dwDebugEventCode == EXCEPTION_DEBUG_EVENT:
+                # Obtain the excpetion code
+                exception = debug_event.u.Exception.ExceptionRecord.ExceptionCode
+                self.exception_address = debug_event.u.Exception.ExceptionRecord.ExceptionAddress
+
+                if exception == EXCEPTION_ACCESS_VIOLATION:
+                    print("Access violation detected.")
+
+                # If a breakpoint is detected we call an internal handler
+                elif exception == EXCEPTION_BREAKPOINT:
+                    continue_status = self.exception_handler_breakpoint()
+
+                elif exception == EXCEPTION_GUARD_PAGE:
+                    print("Guard page access detected")
+
+                elif exception == EXCEPTION_SINGLE_STEP:
+                    print("Single stepping")
+
             kernel32.ContinueDebugEvent(debug_event.dwProcessId,
                                         debug_event.dwThreadId,
                                         continue_status)
@@ -135,3 +161,8 @@ class debugger():
             return context
         else:
             return False
+
+    def exception_handler_breakpoint(self):
+        print("[*] Inside the breakpoint handler")
+        print("Exception address: 0x%08x" % self.exception_address)
+        return DBG_CONTINUE
--- chapter3/mydebugger/my_test.py
+++ chapter3/mydebugger/my_test.py
@@ -7,21 +7,23 @@ pid = input("Enter the PID of the process to attach to: ")
 
 debugger.attach(int(pid))
 
-list = debugger.enumerate_threads()
+# list = debugger.enumerate_threads()
+#
+# # For each thread in the list we want to
+# # grab the value of each of the registers
+# for thread in list:
+#     thread_context = debugger.get_thread_context(thread)
+#     # Now let's output the contents of some of the registers
+#     print("[*] Dumping registers for thread ID: 0x%08x" % thread)
+#     print("[**] RSI: 0x%08x" % thread_context.Rsi)
+#     print("[**] RDI: 0x%08x" % thread_context.Rdi)
+#     print("[**] RBP: 0x%08x" % thread_context.Rbp)
+#     print("[**] RAX: 0x%08x" % thread_context.Rax)
+#     print("[**] RBX: 0x%08x" % thread_context.Rbx)
+#     print("[**] RCX: 0x%08x" % thread_context.Rcx)
+#     print("[**] RDX: 0x%08x" % thread_context.Rdx)
+# print("[*] END DUMP")
 
-# For each thread in the list we want to
-# grab the value of each of the registers
-for thread in list:
-    thread_context = debugger.get_thread_context(thread)
-    # Now let's output the contents of some of the registers
-    print("[*] Dumping registers for thread ID: 0x%08x" % thread)
-    print("[**] RSI: 0x%08x" % thread_context.Rsi)
-    print("[**] RDI: 0x%08x" % thread_context.Rdi)
-    print("[**] RBP: 0x%08x" % thread_context.Rbp)
-    print("[**] RAX: 0x%08x" % thread_context.Rax)
-    print("[**] RBX: 0x%08x" % thread_context.Rbx)
-    print("[**] RCX: 0x%08x" % thread_context.Rcx)
-    print("[**] RDX: 0x%08x" % thread_context.Rdx)
-print("[*] END DUMP")
+debugger.run()
 
 debugger.detach()
